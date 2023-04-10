1dee730e7cfa81e7cab489769c7ee8b9ddd0b271
Lucas Coster <93486883+lucas22c@users.noreply.github.com>
2023-04-10 17:32:09 -0400

    Add files via upload

--- DataStoring.py
+++ DataStoring.py
@@ -1,16 +1,53 @@
+import pandas as pd
 import numpy as np
 import h5py
 
-with h5py.File('./hdf5_data.h5', 'w') as hdf:
+firstMemberWalking = pd.read_csv('HaydenMurphyWalkingData.csv')
 
-    name1 = hdf.create_group('/Lucas Coster')
-    name1.create_dataset('Walking', 'LucasCosterWalkingData.csv')
-    name1.create_dataset('Jumping', 'LucasCosterJumpingData.csv')
+firstMemberJumping = pd.read_csv('HaydenMurphyJumpingData.csv')
 
-    name2 = hdf.create_group('/Hayden Murphy')
-    name1.create_dataset('Walking', 'HaydenMurphyWalkingData.csv')
-    name1.create_dataset('Jumping', 'HaydenMurphyJumpingData.csv')
+secondMemberWalking = pd.read_csv('JacobHicklingWalkingData.csv')
 
-    name3 = hdf.create_group('/Jacob Hickling')
-    name1.create_dataset('Walking', 'JacobHicklingWalkingData.csv')
-    name1.create_dataset('Jumping', 'JacobHicklingJumpingData.csv')
+secondMemberJumping = pd.read_csv('JacobHicklingJumpingData.csv')
+
+thirdMemberWalking = pd.read_csv('LucasCosterWalkingData.csv')
+
+thirdMemberJumping = pd.read_csv('LucasCosterJumpingData.csv')
+
+walkingJoined = pd.concat([firstMemberWalking, secondMemberWalking, thirdMemberWalking], ignore_index=True)
+jumpingJoined = pd.concat([firstMemberJumping, secondMemberJumping, thirdMemberJumping], ignore_index=True)
+allJoined = pd.concat([walkingJoined, jumpingJoined], ignore_index=True)
+
+with h5py.File('./project_data.h5', 'w') as hdf:
+    Hayden = hdf.create_group('Hayden Murphy')
+    Hayden.create_dataset('walking', data=firstMemberWalking)
+    Hayden.create_dataset('jumping', data=firstMemberJumping)
+
+    Jacob = hdf.create_group('Jacob Hickling')
+    Jacob.create_dataset('walking', data=secondMemberWalking)
+    Jacob.create_dataset('jumping', data=secondMemberJumping)
+
+    Lucas = hdf.create_group('Lucas Coster')
+    Lucas.create_dataset('walking', data=thirdMemberWalking)
+    Lucas.create_dataset('jumping', data=thirdMemberJumping)
+
+    dataset_group = hdf.create_group('dataset')
+
+    num_segments = (len(allJoined) - 500) // 100 + 1
+    segments = [allJoined[(i * 100):(i * 100 + 500)] for i in range(num_segments)]
+
+    np.random.shuffle(segments)
+
+    nTrain = int(0.9 * len(segments))
+    train_segments = segments[:nTrain]
+    test_segments = segments[nTrain:]
+
+    test_group = hdf.create_group('Test')
+    train_group = hdf.create_group('Train')
+
+    test_group.create_dataset('test', data=test_segments)
+    train_group.create_dataset('train', data=train_segments)
+
+with h5py.File('./project_data.h5', 'r') as hdf:
+    ls = list(hdf.keys())
+    print(ls)
