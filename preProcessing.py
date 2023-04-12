3c37ba3fccb10881721b2c0b462358013a76e2e6
jacobhickling <jacobhickling14@gmail.com>
2023-04-11 21:29:32 -0400

    updates that might be better? still doesnt work yet

--- preProcessing.py
+++ preProcessing.py
@@ -1,125 +1,55 @@
 import pandas as pd
-import matplotlib.pyplot as plt
 import h5py
 import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import accuracy_score
-from sklearn.pipeline import make_pipeline
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import roc_curve
-from sklearn.metrics import RocCurveDisplay
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import ConfusionMatrixDisplay
-from sklearn.metrics import roc_auc_score
-from sklearn.metrics import recall_score
-from scipy.stats import skew
-import joblib
+from scipy.stats import skew, kurtosis, entropy
 
 
-def preprocessing(windows, window_size):
-    filtered = np.zeros((windows.shape[0], windows.shape[1] - window_size + 1, windows.shape[2]))
-    for i in range(windows.shape[0]):
-        x_frame = pd.DataFrame(windows[i, :, 0])
-        y_frame = pd.DataFrame(windows[i, :, 1])
-        z_frame = pd.DataFrame(windows[i, :, 2])
-        total = windows[i, :, 3]
+def features(segment):
 
-        # apply MA
-
-        x_ma = x_frame.rolling(window_size).mean().values.ravel()
-        y_ma = y_frame.rolling(window_size).mean().values.ravel()
-        z_ma = z_frame.rolling(window_size).mean().values.ravel()
-
-        # discard NaN
-
-        x_ma = x_ma[window_size - 1:]
-        y_ma = y_ma[window_size - 1:]
-        z_ma = z_ma[window_size - 1:]
-        total_ma = total[window_size - 1:]
-
-        filtered[i, :, 0] = x_ma
-        filtered[i, :, 1] = y_ma
-        filtered[i, :, 2] = z_ma
-        filtered[i, :, 3] = total_ma
-
-    feature_data = train_feature_extract(filtered)
-
-    x_frame = pd.DataFrame(feature_data[i, :, 0])
-    y_frame = pd.DataFrame(feature_data[i, :, 1])
-    z_frame = pd.DataFrame(feature_data[i, :, 2])
-    total_frame = pd.DataFrame(feature_data[i, :, 3])
-
-    for frame in [x_frame, y_frame, z_frame, total_frame]:
-        for i in range(frame.shape[0]):
-            column =
+    # creates dictionary of features for a segment
+    feature = {"maximum": np.max(segment),
+               "minimum": np.min(segment),
+               "range": np.max(segment) - np.min(segment),
+               "mean": np.mean(segment),
+               "median": np.median(segment),
+               "variance": np.var(segment),
+               "skewness": skew(segment),
+               "std": np.std(segment),
+               "kurtosis": kurtosis(segment),
+               "entropy": entropy(segment)}
+    return feature
 
 
-def train_feature_extract(windows):
-    train_features = np.zeros((windows.shape[0], 10, 4))
-    for i in range(windows.shape[2]):
-        for j in range(windows.shape[0]):
-            window_data = windows[j, :, i]
+with h5py.File("./project_data.h5", "r") as f:
+    walk_train = pd.DataFrame(f["/Dataset/Train/Walking"])
+    walk_test = pd.DataFrame(f["/Dataset/Test/Walking"])
+    jump_train = pd.DataFrame(f["/Dataset/Train/Jumping"])
+    jump_test = pd.DataFrame(f["/Dataset/Test/Jumping"])
 
-            max_val = np.max(window_data)
-            min_val = np.min(window_data)
-            range_val = max_val - min_val
-            mean_val = np.mean(window_data)
-            median_val = np.median(window_data)
-            var_val = np.var(window_data)
-            skew_val = skew(window_data)
-            rms_val = np.sqrt(np.mean(window_data ** 2))
-            kurt_val = np.mean((window_data - np.mean(window_data)) ** 4) / (np.var(window_data) ** 2)
-            std_val = np.std(window_data)
+    walk_train.columns = ["time", "x acceleration", "y acceleration", "z acceleration", "abs acceleration"]
+    walk_test.columns = ["time", "x acceleration", "y acceleration", "z acceleration", "abs acceleration"]
+    jump_train.columns = ["time", "x acceleration", "y acceleration", "z acceleration", "abs acceleration"]
+    jump_test.columns = ["time", "x acceleration", "y acceleration", "z acceleration", "abs acceleration"]
 
-            train_features[j, :, i] = (max_val, min_val, range_val, mean_val, median_val, var_val, skew_val,
-                                       rms_val, kurt_val, std_val)
+    f.close()
 
-    return train_features
+walk_train.drop(["time"])
+walk_train.fillna(walk_train.mean(), inplace=True)
+walk_train.rolling(5, min_periods=1).mean()
 
+fig, ax = plt.subplots(figsize=(10, 10))
+ax.plot(walk_train, linewidth=5)
+plt.show()
 
-def test_feature_extract(windows):
+# dataframes are structured as [rows: segments, cols: slot of segment, z-dim: type of acceleration]
 
-    # Create an empty array to hold the feature vectors
+jump_train.drop(["time"])
+jump_train.fillna(walk_train.mean(), inplace=True)
+jump_train.rolling(5, min_periods=1).mean()
 
-    test_features = np.zeros((windows.shape[0], 10, 4))
-
-    # Iterate over each time window and extract the features
-    for i in range(windows.shape[2]):
-        for j in range(windows.shape[0]):
-            # Extract the data from the window
-            window_data = windows[j, :, i]
-
-            # Compute the features
-            max_val = np.max(window_data)
-            min_val = np.min(window_data)
-            range_val = max_val - min_val
-            mean_val = np.mean(window_data)
-            median_val = np.median(window_data)
-            var_val = np.var(window_data)
-            skew_val = skew(window_data)
-            rms_val = np.sqrt(np.mean(window_data ** 2))
-            kurt_val = np.mean((window_data - np.mean(window_data)) ** 4) / (np.var(window_data) ** 2)
-            std_val = np.std(window_data)
-
-            # Store the features in the features array
-            test_features[j, :, i] = (max_val, min_val, range_val, mean_val, median_val, var_val, skew_val, rms_val,
-                                 kurt_val, std_val)
-
-    x_feature = test_features[:, :, 0]
-    y_feature = test_features[:, :, 1]
-    z_feature = test_features[:, :, 2]
-    total_feature = test_features[:, :, 3]
-
-    # Concatenate the feature arrays
-    all_features = np.concatenate((x_feature, y_feature, z_feature, total_feature), axis=0)
-
-    # Create a column of labels
-    labels = np.concatenate((np.ones((x_feature.shape[0], 1)),
-                             2 * np.ones((y_feature.shape[0], 1)),
-                             3 * np.ones((z_feature.shape[0], 1)),
-                             4 * np.ones((total_feature.shape[0], 1))), axis=0)
-
-    # Add the labels column to the feature array
-    all_features = np.hstack((all_features, labels))
-
-    return all_features
+# normalize!!
+scaler = StandardScaler()
+walk_train = scaler.fit_transform(walk_train)
+jump_train = scaler.fit_transform(jump_train)
