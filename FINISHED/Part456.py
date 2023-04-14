f633e162feb6fd0aea1c10d1919c17b86eefbca5
jacobhickling <jacobhickling14@gmail.com>
2023-04-14 13:25:40 -0400

    Finished?

--- FINISHED/Part456.py
+++ FINISHED/Part456.py
@@ -2,9 +2,12 @@ import pandas as pd
 import h5py
 import numpy as np
 import matplotlib.pyplot as plt
+
+# for preprocessing and feature extraction
 from sklearn.preprocessing import StandardScaler
-from scipy.stats import skew, kurtosis, mode
-from scipy.fft import fft
+from scipy.stats import skew, kurtosis
+
+# for classifier
 from sklearn.metrics import roc_curve
 from sklearn.metrics import roc_auc_score
 from sklearn.metrics import confusion_matrix
@@ -16,10 +19,12 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.pipeline import make_pipeline
 from sklearn.model_selection import LearningCurveDisplay, learning_curve
 import joblib
+
+# allows for easy testing by changing window size here
 window = 5
 
 
-with h5py.File('./project_data.h5', 'r') as hdf:
+with h5py.File('../project_data.h5', 'r') as hdf:
     walk_train = hdf['Dataset/Train/Walking'][:, :, 1:]  # drop time, as it is doesn't matter
     jump_train = hdf['Dataset/Train/Jumping'][:, :, 1:]
     walk_test = hdf['Dataset/Test/Walking'][:, :, 1:]
@@ -27,42 +32,47 @@ with h5py.File('./project_data.h5', 'r') as hdf:
 
 walk_window_count = walk_train.shape[0]
 jump_window_count = jump_train.shape[0]
-
 walk_test_window_count = walk_test.shape[0]
 jump_test_window_count = jump_test.shape[0]
 
 # ---------------------------------------
 # WALKING TRAIN
 
-walking = np.zeros((walk_train.shape[0], walk_train.shape[1] - window + 1, walk_train.shape[2]))
-walk_features_list = np.zeros((walk_window_count, 10, 4))
+walking = np.zeros((walk_window_count, walk_train.shape[1] - window + 1, walk_train.shape[2]))
+walk_features_list = np.zeros((walk_window_count, 10, 4))  # stores features for classifier
+
+fig, ax = plt.subplots()
+ax.plot(walk_train[1, :, 3], color = 'green', label = 'Walking Before')
 
 for i in range(walk_window_count):
+    # x acceleration moving average
     x_acceleration = pd.DataFrame(walk_train[i, :, 0])
     x_acceleration = x_acceleration.rolling(window).mean().values.ravel()
     x_acceleration = x_acceleration[window - 1:]
 
+    # y acceleration moving average
     y_acceleration = pd.DataFrame(walk_train[i, :, 1])
     y_acceleration = y_acceleration.rolling(window).mean().values.ravel()
     y_acceleration = y_acceleration[window - 1:]
 
+    # z acceleration moving average
     z_acceleration = pd.DataFrame(walk_train[i, :, 2])
     z_acceleration = z_acceleration.rolling(window).mean().values.ravel()
     z_acceleration = z_acceleration[window - 1:]
 
+    # absolute acceleration moving average
     abs_acceleration = pd.DataFrame(walk_train[i, :, 3])
     abs_acceleration = abs_acceleration.rolling(window).mean().values.ravel()
     abs_acceleration = abs_acceleration[window - 1:]
 
+    # complete acceleration array
     walking[i, :, 0] = x_acceleration
     walking[i, :, 1] = y_acceleration
     walking[i, :, 2] = z_acceleration
     walking[i, :, 3] = abs_acceleration
 
+    # compute features
     for j in range(4):
-        # print("walking shape " + str((walking[i, :, j]).shape))
-        # print("walk features list shape " + str(walk_features_list[i, :, j].shape))
-        # walk_features_list[i, :, j] = features(walking[i, :, j])
         walk_features_list[i, 0, j] = np.max(walking[i, :, j])
         walk_features_list[i, 1, j] = np.min(walking[i, :, j])
         walk_features_list[i, 2, j] = np.ptp(walking[i, :, j])
@@ -74,49 +84,60 @@ for i in range(walk_window_count):
         walk_features_list[i, 8, j] = kurtosis(walking[i, :, j])
         walk_features_list[i, 9, j] = np.sqrt(np.mean(walking[i, :, j]) ** 2)
 
+# plot comparison
+ax.plot(walking[1, :, 3], color = 'yellow', label = 'Walking After')
+ax.set_title('Acceleration Magnitude vs Time For One Segment')
+ax.set_ylabel('Acceleration Magnitude (in m/s^2)')
+ax.set_xlabel('Time (in 1/100 s)')
+ax.legend()
+plt.show()
+
+# concatenate all features
 walk_features = np.concatenate((pd.DataFrame(walk_features_list[:, :, 0]),
                                 pd.DataFrame(walk_features_list[:, :, 1]),
                                 pd.DataFrame(walk_features_list[:, :, 2]),
                                 pd.DataFrame(walk_features_list[:, :, 3])),
                                axis=0)
 
-column_names = ["max", "min", "range", "mean", "median", "var", "skew", "std", "kurtosis", "rms"]
+# labels for acceleration type for readability
 x_accel_array = np.array([np.array(['x_accel' for _ in range(walk_window_count)], dtype=object)])
 y_accel_array = np.array([np.array(['y_accel' for _ in range(walk_window_count)], dtype=object)])
 z_accel_array = np.array([np.array(['z_accel' for _ in range(walk_window_count)], dtype=object)])
 abs_accel_array = np.array([np.array(['abs_accel' for _ in range(walk_window_count)], dtype=object)])
-
-# x_accel_array = np.empty((walk_window_count, 10))
-# for j in range(walk_window_count):
-#   for k in range(10):
-#       x_accel_array[j, k]
-
 label_column = np.hstack(
     (x_accel_array, y_accel_array, z_accel_array, abs_accel_array),
-    # axis=1  # stack vertically
 )
 
+# all features
+# we rotate because I couldn't figure out how to make the data properly aligned otherwise
 walk_features = np.hstack((walk_features, np.rot90(label_column)))
 
 # ---------------------------------------
 # JUMPING TRAIN
 
-jumping = np.zeros((jump_train.shape[0], jump_train.shape[1] - window + 1, jump_train.shape[2]))
-jump_features_list = np.zeros((jump_window_count, 10, 4))
+# SAME PROCESS AS WALKING, SO I'LL LEAVE OUT COMMENTS
+
+# jumping will store the moving average result
+jumping = np.zeros((jump_window_count, jump_train.shape[1] - window + 1, jump_train.shape[2]))  # must subtract window+1
+jump_features_list = np.zeros((jump_window_count, 10, 4))  # stores features for classifier
 
 for i in range(jump_window_count):
+    # x acceleration moving average
     x_acceleration = pd.DataFrame(jump_train[i, :, 0])
     x_acceleration = x_acceleration.rolling(window).mean().values.ravel()
     x_acceleration = x_acceleration[window - 1:]
 
+    # y acceleration moving average
     y_acceleration = pd.DataFrame(jump_train[i, :, 1])
     y_acceleration = y_acceleration.rolling(window).mean().values.ravel()
     y_acceleration = y_acceleration[window - 1:]
 
+    # z acceleration moving average
     z_acceleration = pd.DataFrame(jump_train[i, :, 2])
     z_acceleration = z_acceleration.rolling(window).mean().values.ravel()
     z_acceleration = z_acceleration[window - 1:]
 
+    # absolute acceleration moving average
     abs_acceleration = pd.DataFrame(jump_train[i, :, 3])
     abs_acceleration = abs_acceleration.rolling(window).mean().values.ravel()
     abs_acceleration = abs_acceleration[window - 1:]
@@ -144,12 +165,10 @@ jump_features = np.concatenate((pd.DataFrame(jump_features_list[:, :, 0]),
                                 pd.DataFrame(jump_features_list[:, :, 3])),
                                axis=0)
 
-column_names = ["max", "min", "range", "mean", "median", "var", "skew", "std", "kurtosis", "rms"]
 x_accel_array = np.array([np.array(['x_accel' for _ in range(jump_window_count)], dtype=object)])
 y_accel_array = np.array([np.array(['y_accel' for _ in range(jump_window_count)], dtype=object)])
 z_accel_array = np.array([np.array(['z_accel' for _ in range(jump_window_count)], dtype=object)])
 abs_accel_array = np.array([np.array(['abs_accel' for _ in range(jump_window_count)], dtype=object)])
-
 label_column = np.hstack(
     (x_accel_array, y_accel_array, z_accel_array, abs_accel_array),
 )
@@ -159,13 +178,12 @@ jump_features = np.hstack((jump_features, np.rot90(label_column)))
 # ---------------------------------------
 # WALKING TEST
 
-walk_test_features_list = np.zeros((walk_test_window_count, 10, 4))
+walk_test_features_list = np.zeros((walk_test_window_count, 10, 4))  # stores features for classifier
 
+# don't bother doing moving average, we want to compare rough data to see how well it works without normalization
 for i in range(walk_test_window_count):
+    # compute features for all windows
     for j in range(4):
-        # print("walking shape " + str((walking[i, :, j]).shape))
-        # print("walk features list shape " + str(walk_features_list[i, :, j].shape))
-        # walk_features_list[i, :, j] = features(walking[i, :, j])
         walk_test_features_list[i, 0, j] = np.max(walk_test[i, :, j])
         walk_test_features_list[i, 1, j] = np.min(walk_test[i, :, j])
         walk_test_features_list[i, 2, j] = np.ptp(walk_test[i, :, j])
@@ -183,20 +201,12 @@ walk_test_features = np.concatenate((pd.DataFrame(walk_test_features_list[:, :, 
                                      pd.DataFrame(walk_test_features_list[:, :, 3])),
                                     axis=0)
 
-column_names = ["max", "min", "range", "mean", "median", "var", "skew", "std", "kurtosis", "rms"]
 x_accel_array = np.array([np.array(['x_accel' for _ in range(walk_test_window_count)], dtype=object)])
 y_accel_array = np.array([np.array(['y_accel' for _ in range(walk_test_window_count)], dtype=object)])
 z_accel_array = np.array([np.array(['z_accel' for _ in range(walk_test_window_count)], dtype=object)])
 abs_accel_array = np.array([np.array(['abs_accel' for _ in range(walk_test_window_count)], dtype=object)])
-
-# x_accel_array = np.empty((walk_window_count, 10))
-# for j in range(walk_window_count):
-#   for k in range(10):
-#       x_accel_array[j, k]
-
 label_column = np.hstack(
     (x_accel_array, y_accel_array, z_accel_array, abs_accel_array),
-    # axis=1  # stack vertically
 )
 
 walk_test_features = np.hstack((walk_test_features, np.rot90(label_column)))
@@ -204,7 +214,9 @@ walk_test_features = np.hstack((walk_test_features, np.rot90(label_column)))
 # ---------------------------------------
 # JUMPING TEST
 
-jump_test_features_list = np.zeros((jump_test_window_count, 10, 4))
+# SAME PROCESS AS WALKING, SO NO COMMENTS AGAIN
+
+jump_test_features_list = np.zeros((jump_test_window_count, 10, 4))  # stores features for classifier
 
 for i in range(jump_test_window_count):
     for j in range(4):
@@ -225,34 +237,30 @@ jump_test_features = np.concatenate((pd.DataFrame(jump_test_features_list[:, :, 
                                      pd.DataFrame(jump_test_features_list[:, :, 3])),
                                     axis=0)
 
-column_names = ["max", "min", "range", "mean", "median", "var", "skew", "std", "kurtosis", "rms"]
 x_accel_array = np.array([np.array(['x_accel' for _ in range(jump_test_window_count)], dtype=object)])
 y_accel_array = np.array([np.array(['y_accel' for _ in range(jump_test_window_count)], dtype=object)])
 z_accel_array = np.array([np.array(['z_accel' for _ in range(jump_test_window_count)], dtype=object)])
 abs_accel_array = np.array([np.array(['abs_accel' for _ in range(jump_test_window_count)], dtype=object)])
-
-# x_accel_array = np.empty((walk_window_count, 10))
-# for j in range(walk_window_count):
-#   for k in range(10):
-#       x_accel_array[j, k]
-
 label_column = np.hstack(
     (x_accel_array, y_accel_array, z_accel_array, abs_accel_array),
-    # axis=1  # stack vertically
 )
 
 jump_test_features = np.hstack((jump_test_features, np.rot90(label_column)))
 
-column_names = np.array(["max", "min", "range", "mean", "median", "var", "skew", "std", "kurtosis", "rms", "measurement", "activity"])
+# -------------------------------------------
+# columns for readability!!
+column_names = np.array(["max", "min", "range", "mean", "median", "var", "skew", "std", "kurtosis", "rms", "type", "walk/jump"])
 
+# features, labels, and concatenations
 train_features = np.concatenate((walk_features, jump_features))
-train_labels = np.concatenate((np.zeros((walk_features.shape[0], 1)), np.ones((jump_features.shape[0], 1))))
-training = pd.DataFrame(np.hstack((train_features, train_labels)), columns=column_names)
-
 test_features = np.concatenate((walk_test_features, jump_test_features))
+train_labels = np.concatenate((np.zeros((walk_features.shape[0], 1)), np.ones((jump_features.shape[0], 1))))
 test_labels = np.concatenate((np.zeros((walk_test_features.shape[0], 1)), np.ones((jump_test_features.shape[0], 1))))
+training = pd.DataFrame(np.hstack((train_features, train_labels)), columns=column_names)
 testing = pd.DataFrame(np.hstack((test_features, test_labels)), columns=column_names)
+
 # FEATURES DONE!!!
+# ----------------------------------------------
 
 # normalize!!
 scaler = StandardScaler()
@@ -265,6 +273,8 @@ y_train = y_train.astype('int')
 X_test = testing.iloc[:, 0:-2]
 y_test = testing.iloc[:, -1]
 y_test = y_test.astype('int')
+
+# effectively identical to lab 6 below
 
 l_reg = LogisticRegression(max_iter=10000)
 clf = make_pipeline(scaler, l_reg)
